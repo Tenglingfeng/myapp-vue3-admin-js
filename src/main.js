b520afdb69490a4145871b14259a727383deafea
Teng <704279465@qq.com>
2021-10-28 21:39:13 +0800

    引入antdesign , 添加vue.config.js 配置文件

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+//and-design-vue
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+// eslint-disable-next-line prettier/prettier
+createApp(App)
+.use(store)
+.use(router)
+.use(Antd)
+.mount("#app");
--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="login">
     <h1>登录</h1>
+    <a-button type="primary">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    <a-button type="dashed">Dashed Button</a-button>
+    <a-button type="text">Text Button</a-button>
+    <a-button type="link">Link Button</a-button>
   </div>
 </template>
 
