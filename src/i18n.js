7f5531a8f5b947897de5f540d5624e38c8235409
tenglingfeng <704279465@qq.com>
2021-11-04 00:44:01 +0800

    I18n测试

--- src/i18n.js
+++ src/i18n.js
@@ -24,7 +24,9 @@ function loadLocaleMessages() {
 }
 
 export default createI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || "en",
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
+  //locale: process.env.VUE_APP_I18N_LOCALE || "en",
+  //fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
+  locale: "ch",
+  fallbackLocale: "ch",
   messages: loadLocaleMessages(),
 });
