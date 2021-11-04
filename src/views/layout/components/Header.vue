a3cae5fbf9a63d86b4f7f60370bf0488e310fa01
tenglingfeng <704279465@qq.com>
2021-11-04 22:28:14 +0800

    i18n 语言切换

--- src/views/layout/components/Header.vue
+++ src/views/layout/components/Header.vue
@@ -8,9 +8,25 @@
         </a>
         <template #overlay>
           <a-menu @click="onClick">
-            <a-menu-item key="1">1st menu item</a-menu-item>
-            <a-menu-item key="2">{{ $t("hello") }}</a-menu-item>
-            <a-menu-item key="3">{{ $t("header_menu.logout") }}</a-menu-item>
+            <a-menu-item class="menu-item" key="1">{{
+              $t("header_menu.setting")
+            }}</a-menu-item>
+            <a-menu-item class="menu-item" key="2">{{
+              $t("header_menu.info")
+            }}</a-menu-item>
+            <a-menu-item class="menu-item" key="3">{{
+              $t("header_menu.logout")
+            }}</a-menu-item>
+            <a-menu-item class="menu-item menu-lang" key="4">
+              <span
+                v-for="lang in data.lang"
+                :key="lang.value"
+                @click="toggleLang(lang.value)"
+                :class="{ current: data.lang_current == lang.value }"
+              >
+                {{ lang.lable }}
+              </span>
+            </a-menu-item>
           </a-menu>
         </template>
       </a-dropdown>
@@ -19,8 +35,30 @@
 </template>
 
 <script>
+import { reactive } from "@vue/reactivity";
+import { useI18n } from "vue-i18n";
 export default {
   name: "header",
+  setup() {
+    const { locale } = useI18n({ useScope: "global" });
+    const data = reactive({
+      lang: [
+        { lable: "中文", value: "ch" },
+        { lable: "En", value: "en" },
+      ],
+      lang_current: "ch",
+    });
+
+    const toggleLang = (value) => {
+      locale.value = value;
+      data.lang_current = value;
+    };
+
+    return {
+      data,
+      toggleLang,
+    };
+  },
 };
 </script>
 
@@ -32,12 +70,38 @@ export default {
 .header-menu {
   float: right;
 }
+.menu-item {
+  padding: 0 20px;
+  font-size: 14px;
+  color: #333333;
+  font-family: "Courier New";
+}
+.menu-lang {
+  span {
+    margin-right: 10px;
+  }
+  .current {
+    color: rgb(41, 165, 165);
+  }
+}
 </style>
 
 <i18n>
 {
   "en": {
-    "hello": "Hello i18n in SFC!"
-  }
+    "header_menu":{
+      "logout":"logout",
+      "info":"info",
+      "setting":"setting"
+    },
+    
+  },
+  "ch":{
+     "header_menu":{
+      "logout":"退出",
+      "info":"资料",
+      "setting":"设置"
+     } 
+    }
 }
 </i18n>
