2af52c6b6b8e92cabdb8536abc4caec1be0258af
tenglingfeng <704279465@qq.com>
2021-11-20 14:41:31 +0800

    用户列表页面添加 新增用户子组件开发

---
+++ src/components/Modal/User.vue
@@ -0,0 +1,53 @@
+<!-- 用户 -->
+<template>
+  <a-modal
+    v-model:visible="visible"
+    title="新增用户"
+    @ok="handleOk"
+    @cancel="close"
+  >
+  </a-modal>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ["update:show"],
+  setup(props, context) {
+    const visible = ref(false);
+    console.log(props.show);
+    watch(
+      () => props.show,
+      (newVaule, oldValue) => {
+        console.log(newVaule);
+        console.log(oldValue);
+        visible.value = newVaule;
+      }
+    );
+    const handleOk = (e) => {
+      console.log(e);
+      visible.value = false;
+    };
+
+    const close = () => {
+      console.log("11");
+      context.emit("update:show", false);
+      console.log("2");
+    };
+    return {
+      handleOk,
+      visible,
+      close,
+    };
+  },
+};
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+</style>
--- src/views/admin/User.vue
+++ src/views/admin/User.vue
@@ -1,126 +1,155 @@
-/**用户 */
 <template>
-  <a-table
-    :columns="columns"
-    :data-source="data"
-    :row-selection="{
-      selectedRowKeys: selectedRowKeys,
-      onChange: onSelectChange,
-    }"
-  >
-    <template #name="{ text }">
-      <a>{{ text }}</a>
-    </template>
-    <template #customTitle>
-      <span>
-        <smile-outlined />
-        Name
-      </span>
-    </template>
-    <template #tags="{ text: tags }">
-      <span>
-        <a-tag
-          v-for="tag in tags"
-          :key="tag"
-          :color="
-            tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'
-          "
-        >
-          {{ tag.toUpperCase() }}
-        </a-tag>
-      </span>
-    </template>
-    <template #action="{ record }">
-      <span>
-        <a>Invite 一 {{ record.name }}</a>
-        <a-divider type="vertical" />
-        <a>Delete</a>
-        <a-divider type="vertical" />
-        <a class="ant-dropdown-link">
-          More actions
-          <down-outlined />
-        </a>
-      </span>
-    </template>
-  </a-table>
+  <div class="mt_28">
+    <div class="table-operations">
+      <a-row type="flex">
+        <a-col flex="auto">
+          <a-form
+            layout="inline"
+            :model="formState"
+            :label-col="labelCol"
+            :wrapper-col="wrapperCol"
+          >
+            <a-form-item label="机构">
+              <a-input v-model:value="formState.resource" />
+            </a-form-item>
+
+            <a-form-item label="关键字">
+              <a-input v-model:value="formState.name" />
+            </a-form-item>
+            <a-form-item label="启用状态">
+              <a-select
+                ref="select"
+                v-model:value="value1"
+                style="width: 120px"
+                @focus="focus"
+                @change="handleChange"
+                dropdownMatchSelectWidth="true"
+              >
+                <a-select-option value="jack">Jack</a-select-option>
+                <a-select-option value="lucy">Lucy</a-select-option>
+                <a-select-option value="Yiminghe">yiminghe</a-select-option>
+              </a-select>
+            </a-form-item>
+
+            <a-form-item class="ml_10 mr_10 form_btn">
+              <a-button type="primary">搜索</a-button>
+              <a-button type="primary">重置</a-button>
+              <a-button type="primary">导出</a-button>
+            </a-form-item>
+          </a-form>
+        </a-col>
+        <a-col flex="100px">
+          <a-button type="primary" @click="visible = true">新增用户</a-button>
+        </a-col>
+      </a-row>
+    </div>
+    <a-table
+      :row-selection="{
+        selectedRowKeys: selectedRowKeys,
+        onChange: onSelectChange,
+      }"
+      :columns="columns"
+      :data-source="data"
+      class="mt_28"
+    >
+      <template #tags="{ text: tags }">
+        <span>
+          <a-tag
+            v-for="tag in tags"
+            :key="tag"
+            :color="
+              tag === 'loser'
+                ? 'volcano'
+                : tag.length > 5
+                ? 'geekblue'
+                : 'green'
+            "
+          >
+            {{ tag.toUpperCase() }}
+          </a-tag>
+        </span>
+      </template>
+      <template #enables="{}">
+        <a-switch v-model:checked="checked" />
+      </template>
+      <template #action="">
+        <span>
+          <a>详情</a>
+          <a-divider type="vertical" />
+          <a>删除</a>
+          <a-divider type="vertical" />
+        </span>
+      </template>
+    </a-table>
+    <ModalUser :show="visible" />
+  </div>
 </template>
 <script>
+import { computed, defineComponent, reactive, ref, toRefs } from "vue";
+
 import { SmileOutlined, DownOutlined } from "@ant-design/icons-vue";
-import { defineComponent, reactive, computed } from "vue";
+
+import ModalUser from "@/components/Modal/User.vue";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+  },
+  {
+    title: "Tags",
+    key: "tags",
+    dataIndex: "tags",
+    slots: {
+      customRender: "tags",
+    },
+  },
+  {
+    title: "Enable",
+    key: "enables",
+    dataIndex: "enables",
+    slots: {
+      customRender: "enables",
+    },
+  },
+  {
+    title: "Action",
+    key: "action",
+    slots: {
+      customRender: "action",
+    },
+  },
+];
+const data = [];
+
+for (let i = 0; i < 46; i++) {
+  data.push({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+    tags: [`Teng`, `Ling`, `Feng`],
+    enables: false,
+  });
+}
 
 export default defineComponent({
+  components: { ModalUser },
   setup() {
-    const columns = reactive([
-      {
-        dataIndex: "username",
-        key: "username",
-        title: "用户名",
-        slots: {
-          customRender: "username",
-        },
-      },
-      {
-        dataIndex: "name",
-        key: "name",
-        slots: {
-          title: "customTitle",
-          customRender: "name",
-        },
-      },
-      {
-        title: "Age",
-        dataIndex: "age",
-        key: "age",
-      },
-      {
-        title: "Address",
-        dataIndex: "address",
-        key: "address",
-      },
-      {
-        title: "Tags",
-        key: "tags",
-        dataIndex: "tags",
-        slots: {
-          customRender: "tags",
-        },
-      },
-      {
-        title: "Action",
-        key: "action",
-        slots: {
-          customRender: "action",
-        },
-      },
-    ]);
-    const data = reactive([
-      {
-        key: "1",
-        name: "John Brown",
-        age: 32,
-        address: "New York No. 1 Lake Park",
-        tags: ["nice", "developer"],
-      },
-      {
-        key: "2",
-        name: "Jim Green",
-        age: 42,
-        address: "London No. 1 Lake Park",
-        tags: ["loser"],
-      },
-      {
-        key: "3",
-        name: "Joe Black",
-        age: 32,
-        address: "Sidney No. 1 Lake Park",
-        tags: ["cool", "teacher"],
-      },
-    ]);
     const state = reactive({
       selectedRowKeys: [],
       // Check here to configure the default column
       loading: false,
     });
+    const hasSelected = computed(() => state.selectedRowKeys.length > 0);
 
     const start = () => {
       state.loading = true; // ajax request after empty completing
@@ -131,24 +160,53 @@ export default defineComponent({
       }, 1000);
     };
 
-    const hasSelected = computed(() => state.selectedRowKeys.length > 0);
-
     const onSelectChange = (selectedRowKeys) => {
       console.log("selectedRowKeys changed: ", selectedRowKeys);
       state.selectedRowKeys = selectedRowKeys;
     };
+    const checked = ref(false);
+
+    const from = reactive({
+      labelCol: {
+        span: 4,
+      },
+      wrapperCol: {
+        span: 14,
+      },
+    });
+    const formState = reactive({
+      name: "",
+      region: undefined,
+      date1: undefined,
+      delivery: false,
+      type: [],
+      resource: "",
+      desc: "",
+    });
+
+    const visible = ref(false);
+
     return {
       data,
       columns,
-      onSelectChange,
       hasSelected,
+      ...toRefs(state),
+      // func
       start,
+      onSelectChange,
+      SmileOutlined,
+      DownOutlined,
+      checked,
+      from,
+      formState,
+      visible,
     };
-  },
-
-  components: {
-    SmileOutlined,
-    DownOutlined,
   },
 });
 </script>
+
+<style scoped>
+.form_btn button {
+  margin-left: 20px;
+}
+</style>
